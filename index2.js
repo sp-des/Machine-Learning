0e822a1f28e9363f603383b44c7fa7e6b7b1b0eb
Spades <33013584+sp-des@users.noreply.github.com>
2019-11-02 18:11:28 +0000

    Update index2.js
    Added a way for a network to be saved after training

--- index2.js
+++ index2.js
@@ -25,3 +25,46 @@ rl.question('> ', (sentence) => {
     console.log(`Category: ${output}`);
     rl.close()
 })
+const brain = require('brain.js');
+const fs = require('fs');
+const data = require('./data.json');
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const networkPath = 'cached.network.json';
+
+const network = new brain.recurrent.LSTM();
+
+const trainingData = data.map(item => ({
+  input: item.text,
+  output: item.category
+}));
+
+
+//network.train(trainingData, { //train the program
+//  iterations: 100
+//});
+
+//cach network
+let networkData = null;
+if (fs.existsSync(networkPath)) {
+  networkData = JSON.parse(fs.readFileSync(networkPath));
+  network.fromJSON(networkData);
+} else {
+  network.train(trainingData, {
+    iterations: 200
+  });
+  fs.writeFileSync(networkPath, JSON.stringify(network.toJSON(), null, 2));
+}
+
+//enter sentence to run
+rl.question('> ', (sentence) => {
+    var splits = sentence.split(",")
+    answer = splits[0]
+    const output = network.run(answer);
+    console.log(`Category: ${output}`);
+    rl.close()
+})
