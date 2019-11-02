bf35f9bdcce9d8938aa2e14ee97b743c0ded6864
Spades <33013584+sp-des@users.noreply.github.com>
2019-11-02 18:25:36 +0000

    Update index2.js
    Added a way for a network to be saved after training

--- index2.js
+++ index2.js
@@ -1,31 +1,4 @@
 const brain = require('brain.js');
-const data = require('./data.json');
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-const network = new brain.recurrent.LSTM();
-
-const trainingData = data.map(item => ({
-  input: item.text,
-  output: item.category
-}));
-
-network.train(trainingData, {
-  iterations: 100
-});
-
-
-rl.question('> ', (sentence) => {
-    var splits = sentence.split(",")
-    answer = splits[0]
-    const output = network.run(answer);
-    console.log(`Category: ${output}`);
-    rl.close()
-})
-const brain = require('brain.js');
 const fs = require('fs');
 const data = require('./data.json');
 const readline = require('readline');
